12b7bd510dd3be1e4d5f4a24bf86581e62533795
Abi amarulloh <abiamarulloh06@gmail.com>
2020-09-28 04:17:19 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send("Running API"))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`server running on ${PORT}`))
